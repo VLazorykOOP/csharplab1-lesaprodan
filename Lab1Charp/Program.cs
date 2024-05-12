39b6ed6d3f03295376db682631310e26a2344239
lesaprodan <99794388+lesaprodan@users.noreply.github.com>
2024-05-12 23:17:04 +0300

    Update Program.cs

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -16,3 +16,30 @@ static void Main(string[] args)
         Console.WriteLine($"Радіус вписаного кола: {radius}");
     }
 }
+//task 2
+class lab12
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть двозначне число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Визначаємо першу і другу цифри числа
+        int firstDigit = number / 10; // отримуємо цифру десятків шляхом ділення на 10
+        int secondDigit = number % 10; // отримуємо цифру одиниць за допомогою операції взяття остачі від ділення на 10
+
+        // Порівнюємо першу і другу цифри числа
+        if (firstDigit > secondDigit)
+        {
+            Console.WriteLine("Перша цифра більша за другу.");
+        }
+        else if (firstDigit < secondDigit)
+        {
+            Console.WriteLine("Друга цифра більша за першу.");
+        }
+        else
+        {
+            Console.WriteLine("Обидві цифри рівні.");
+        }
+    }
+}
